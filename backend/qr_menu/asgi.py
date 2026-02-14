aabc9547f29e4f1a1e189781ead2afde5019a691
Sharipov Jonibek <jsharipov559@gmail.com>
2026-02-15 00:24:50 +0500

    Update config's name

--- backend/qr_menu/asgi.py
+++ backend/qr_menu/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for qr_name project.
+ASGI config for qr_menu project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_menu.settings')
 
 application = get_asgi_application()
--- backend/qr_menu/wsgi.py
+++ backend/qr_menu/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qr_name project.
+WSGI config for qr_menu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_menu.settings')
 
 application = get_wsgi_application()
